a7cd6b25b7b1f0e7b94cbf748fb5ab30b7881cf7
sk00l <saurav.bhattarai078@gmail.com>
2023-01-18 22:36:06 +0545

    nav bar

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Container} from 'react-bootstrap'
+import Header from './components/Header';
+import Footer from './components/Footer';
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to Online shop.</h1>
+
+        </Container>
+      </main>
+      <Footer/>
+  
+    </>
+  );
+}
+
+export default App;
